540f8fa23930c76a06546e233bbb2b4fc636d699
birk <birk@xxx.com>
2020-12-30 11:34:01 +0800

    timestamp类型时区问题修复

---
+++ src/main/java/com/zendesk/maxwell/schema/columndef/TimeStampColumnDef.java
@@ -0,0 +1,50 @@
+package com.zendesk.maxwell.schema.columndef;
+
+
+import com.zendesk.maxwell.producer.MaxwellOutputConfig;
+import com.zendesk.maxwell.util.MySQLConstants;
+
+import java.sql.Timestamp;
+
+public class TimeStampColumnDef extends ColumnDefWithLength {
+	@Override
+	protected String formatValue(Object value, MaxwellOutputConfig config) throws ColumnDefCastException
+	{
+		Timestamp ts = TimeStampFormatter.extractTimestamp(value);
+		String dateString = TimeStampFormatter.formatDateTime(value, ts);
+
+		//System.out.println("3. "+value+" : "+ ts.toString() +" : " + dateString);
+
+		if ( dateString == null )
+			return null;
+		else
+			return appendFractionalSeconds(dateString, ts.getNanos(), columnLength);
+	}
+
+	public TimeStampColumnDef(String name, String type, short pos, Long columnLength) {
+		super(name, type, pos, columnLength);
+	}
+
+	//@Override
+	public boolean matchesMysqlType(int type) {
+		if ( getType().equals("datetime") ) {
+			return type == MySQLConstants.TYPE_DATETIME ||
+				type == MySQLConstants.TYPE_DATETIME2;
+		} else {
+			return type == MySQLConstants.TYPE_TIMESTAMP ||
+				type == MySQLConstants.TYPE_TIMESTAMP2;
+		}
+	}
+
+	/*protected String formatValue(Object value) {
+		Timestamp ts = TimeStampFormatter.extractTimestamp(value);
+		String dateString = TimeStampFormatter.formatDateTime(value, ts);
+
+		//System.out.println("3. "+value+" : "+ ts.toString() +" : " + dateString);
+
+		if ( dateString == null )
+			return null;
+		else
+			return appendFractionalSeconds(dateString, ts.getNanos(), columnLength);
+	}*/
+}
